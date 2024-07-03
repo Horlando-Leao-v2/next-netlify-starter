ff93af7d35380e8bd3367b90e78692bc1d8ad3b5
Horlando-Leao-v2 <horlandojcleao.developer@gmail.com>
2024-07-03 12:51:25 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
     const options = {
       method: 'GET',
       headers: {
-        'Authorization': authToken
+        'Authorization': `Bearer ${authToken}`
       }
     };
 
